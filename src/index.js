42aba524b24e7735768445a74d263c6973c36dee
Lizaveta <elis.tkachuk@gmail.com>
2018-09-20 11:28:33 +0300

    Game, Board, Square classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+class Square extends React.Component
+{
+    render(props)
+    {
+        <button>
+           <div> {this.props.value} </div>
+        </button>
+    }
+}
+class Board extends React.Component
+{
+renderSquare(i)
+{
+    return <Square value={i}/>
+}
+render()
+{
+    const status = "Next player : X";
+    return (
+    <div>
+        <div >{status}</div>
+        <div >
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+        </div>
+        <div >
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+        </div>
+        <div >
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+        </div>
+    </div>
+    )
+}
+}
+class Game extends React.Component
+{
+render(){
+    return
+    (
+        <div>
+            <Board/>
+        </div>
+    )
+    }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+}
+ReactDOM.render(<Game />, document.getElementById('root'));
 registerServiceWorker();
